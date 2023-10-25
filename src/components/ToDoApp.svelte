035bde4ffa5e207c2992ebd4cc5583a9e7384267
MrVlad0567 <mr.vlad0567@gmail.com>
2023-10-25 11:58:05 +0500

    Changed.

---
+++ src/components/ToDoApp.svelte
@@ -0,0 +1,50 @@
+<script>
+    import ToDoControls from "./ToDoControls.svelte";
+    import ToDoItem from "./ToDoItem.svelte";
+
+    const items = [
+        {
+            id: 1,
+            text: 'Some TODO 1',
+            isDone: false
+        },
+        {
+            id: 2,
+            text: 'Some TODO 2',
+            isDone: false
+        }
+    ];
+</script>
+
+<div class="todo-app">
+    <ToDoControls/>
+    <div class="todo-app__field">
+        {#each items as item}
+            <ToDoItem id={item.id} text={item.text} isDone={item.isDone}/>
+        {/each}
+    </div>
+</div>
+
+<style>
+    .todo-app {
+        width: 700px;
+        height: 80%;
+        background: rgb(176, 168, 168);
+        border-radius: 15px;
+        padding: 40px;
+        display: flex;
+        flex-direction: column;
+        gap: 30px;
+    }
+
+    .todo-app__field {
+        background: white;
+        flex-grow: 1;
+        border-radius: 15px;
+        overflow-y: auto;
+        padding: 20px;
+        display: flex;
+        flex-direction: column;
+        gap: 20px;
+    }
+</style>
---
+++ src/components/ToDoItem.svelte
@@ -0,0 +1,38 @@
+<script>
+    export let id;
+    export let text;
+    export let isDone;
+</script>
+
+<div class="todo-item">
+    <input class="todo-item__check" type="checkbox">
+    <span class="todo-item__text">{text}</span>
+    <button class="todo-item__btn">Delete</button>
+</div>
+
+<style>
+    .todo-item {
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+        padding: 15px;
+        background: rgb(231, 153, 153);
+        border-radius: 10px;
+    }
+
+    .todo-item__check {
+        
+    }
+
+    .todo-item__text {
+        flex-grow: 1;
+    }
+
+    .todo-item__btn {
+        border: none;
+        border-radius: 10px;
+        background-color: chocolate;
+        padding: 10px;
+        min-width: 100px;
+    }
+</style>
